8015edf627101b38e2104fc917da004344b79570
RNOVOSELOV <novoselov_roman@mail.ru>
2022-10-04 09:14:25 +0300

    ДЗ + Тесты с сайта

--- lib/main.dart
+++ lib/main.dart
@@ -26,6 +26,10 @@ class MyHomePage extends StatefulWidget {
 }
 
 class _MyHomePageState extends State<MyHomePage> {
+  static const Color buttonColorStart = Color(0xFF40CA88);
+  static const Color buttonColorWait = Color(0xFFE0982D);
+  static const Color buttonColorStop = Color(0xFFE02D47);
+
   String millisecondsText = "";
   GameState gameState = GameState.readyToStart;
   Timer? waitingTimer;
@@ -34,23 +38,23 @@ class _MyHomePageState extends State<MyHomePage> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      backgroundColor: Colors.white,
+      backgroundColor: Color(0xFF282E3D),
       body: Stack(
         children: [
           const Align(
-              alignment: Alignment(0, -0.9),
+              alignment: Alignment(0, -0.8),
               child: Text(
                 "Test your\nreaction speed",
                 textAlign: TextAlign.center,
                 style: TextStyle(
-                    fontSize: 40,
-                    fontWeight: FontWeight.w700,
-                    color: Colors.black),
+                    fontSize: 38,
+                    fontWeight: FontWeight.w900,
+                    color: Colors.white),
               )),
           Align(
               alignment: Alignment.center,
               child: ColoredBox(
-                color: Colors.black12,
+                color: Color(0xFF6D6D6D),
                 child: SizedBox(
                   height: 160,
                   width: 300,
@@ -58,15 +62,15 @@ class _MyHomePageState extends State<MyHomePage> {
                     child: Text(
                       millisecondsText,
                       style: const TextStyle(
-                          fontSize: 40,
-                          fontWeight: FontWeight.w700,
-                          color: Colors.black),
+                          fontSize: 36,
+                          fontWeight: FontWeight.w500,
+                          color: Colors.white),
                     ),
                   ),
                 ),
               )),
           Align(
-              alignment: const Alignment(0, 0.9),
+              alignment: const Alignment(0, 0.8),
               child: GestureDetector(
                 onTap: () => setState(() {
                   switch (gameState) {
@@ -85,7 +89,7 @@ class _MyHomePageState extends State<MyHomePage> {
                   }
                 }),
                 child: ColoredBox(
-                  color: Colors.black12,
+                  color: _getButtonColor (),
                   child: SizedBox(
                     height: 200,
                     width: 200,
@@ -93,9 +97,9 @@ class _MyHomePageState extends State<MyHomePage> {
                       child: Text(
                         _getButtonText(),
                         style: const TextStyle(
-                            fontSize: 35,
-                            fontWeight: FontWeight.w700,
-                            color: Colors.black),
+                            fontSize: 38,
+                            fontWeight: FontWeight.w900,
+                            color: Colors.white),
                       ),
                     ),
                   ),
@@ -114,6 +118,17 @@ class _MyHomePageState extends State<MyHomePage> {
         return "WAIT";
       case GameState.canBeStopped:
         return "STOP";
+    }
+  }
+
+  Color _getButtonColor() {
+    switch (gameState) {
+      case GameState.readyToStart:
+        return buttonColorStart;
+      case GameState.waiting:
+        return buttonColorWait;
+      case GameState.canBeStopped:
+        return buttonColorStop;
     }
   }
 
